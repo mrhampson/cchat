8333dc9379cbc5317ce983e9fa930ad719e04e09
Marshall Hampson <mhampson@liveaction.com>
2018-08-17 11:06:00 -0700

    Reorg files for xcode debugging. Add sender thread

--- cchat/cchat/server.c
+++ cchat/cchat/server.c
@@ -12,9 +12,11 @@
 #define MAX_SOCKS 512
 
 void* handleClient(void* args);
+void* dispatchMessageToAllSocks(void* args);
 
 int main(int argc, char* argv[]) {
-  int allClientDescriptors[MAX_SOCKS] = {0};
+  int allClientDescriptorsIdx = 0;
+  int allClientDescriptors[MAX_SOCKS] = {-1};
   struct sockaddr_in serverAddrInfo, clientAddrInfo;
   int serverDescriptor, clientDescriptor;
 
@@ -27,18 +29,25 @@ int main(int argc, char* argv[]) {
 
   bind(serverDescriptor, (struct sockaddr *)&serverAddrInfo, sizeof(struct sockaddr_in));
 
+  // Create send to all thread
+  pthread_t sendToAllThread;
+  pthread_create(&sendToAllThread, NULL, dispatchMessageToAllSocks, (void*)allClientDescriptors);
+
   listen(serverDescriptor, QLEN);
   printf("Listening ...\n");
   while (1) {
     int len = sizeof(struct sockaddr_in);
     clientDescriptor = accept(serverDescriptor, (struct sockaddr*) &clientAddrInfo, (socklen_t *)&len);
+    allClientDescriptors[allClientDescriptorsIdx] = clientDescriptor;
+    allClientDescriptorsIdx++;
     printf("Got new connection\n");
+    // TODO how to join these threads
     pthread_t clientThread;
-    pthread_create(&clientThread, NULL, handleClient, (void*) &clientDescriptor);
+    pthread_create(&clientThread, NULL, handleClient, (void*)&clientDescriptor);
   }
+  pthread_join(sendToAllThread, NULL);
   return 0;
 }
-
 
 void* handleClient(void* args) {
   int clientDescriptor = *((int *)args);
@@ -47,5 +56,16 @@ void* handleClient(void* args) {
   while(1) {
     recv(clientDescriptor, buf, BLEN, 0);
     send(clientDescriptor, buf, strlen(buf), 0);
+  }
+}
+
+void* dispatchMessageToAllSocks(void* args) {
+  printf("Started dispatching thread for all sockets");
+  int* allSockDesciptors = (int*)args;
+  while(1) {
+    int i = 0;
+    while(i < MAX_SOCKS && allSockDesciptors[i] != -1) {
+      send(allSockDesciptors[i], "Hi\n", strlen("Hi\n"), 0);
+    }
   }
 }
