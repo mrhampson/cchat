ddc4b251e81956cbc03c71c8b0c84ab5249c7c2f
Marshall Hampson <mhampson@liveaction.com>
2018-08-17 02:17:11 -0700

    WIP fork per connection

--- server.c
+++ server.c
@@ -4,19 +4,22 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <pthread/pthread.h>
 
 #define PORT 4897
 #define QLEN 100
-#define BLEN 10
+#define BLEN 256
 
-int main(int argc, char *argv[]) {
+
+void* handleClient(void* args);
+
+int main(int argc, char* argv[]) {
   struct sockaddr_in serverAddrInfo, clientAddrInfo;
   int serverDescriptor, clientDescriptor;
-  char buf[BLEN];
-
+  
   serverDescriptor = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   
-  memset((char *) &serverAddrInfo, 0, sizeof(struct sockaddr_in));
+  memset((char*)&serverAddrInfo, 0, sizeof(struct sockaddr_in));
   serverAddrInfo.sin_family = AF_INET;
   serverAddrInfo.sin_addr.s_addr = INADDR_ANY;
   serverAddrInfo.sin_port = htons((u_short) PORT);
@@ -24,13 +27,24 @@ int main(int argc, char *argv[]) {
   bind(serverDescriptor, (struct sockaddr *)&serverAddrInfo, sizeof(struct sockaddr_in));
 
   listen(serverDescriptor, QLEN);
-
+  printf("Listening ...\n");
   while (1) {
     int len = sizeof(struct sockaddr_in);
-    clientDescriptor = accept(serverDescriptor, (struct sockaddr *) &clientAddrInfo, (socklen_t *)&len);
-    memset(buf, 0, BLEN);
+    clientDescriptor = accept(serverDescriptor, (struct sockaddr*) &clientAddrInfo, (socklen_t *)&len);
+    printf("Got new connection\n");
+    pthread_t clientThread;
+    pthread_create(&clientThread, NULL, handleClient, (void*) &clientDescriptor);
+  }
+  return 0;
+}
+
+
+void* handleClient(void* args) {
+  int clientDescriptor = *((int *)args);
+  printf("Listening to client on descriptor %i\n", clientDescriptor);
+  char buf[BLEN] = {0}; 
+  while(1) {
     recv(clientDescriptor, buf, BLEN, 0);
     send(clientDescriptor, buf, strlen(buf), 0);
   }
-  return 0;
 }
