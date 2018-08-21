c4536f491343b834ad1820f4e56a95bbf906cc7e
Marshall Hampson <mhampson@liveaction.com>
2018-08-21 15:29:03 -0700

    Display sender ip in chat

--- cchat/cchat/server.c
+++ cchat/cchat/server.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,20 +10,27 @@
 
 #define PORT 4000
 #define QLEN 100
-#define BLEN 512
+#define BLEN 1024
 #define MAX_SOCKS 512
 #define POLL_TIMEOUT -1
+
+struct client_thread_info {
+  int descriptor;
+  struct sockaddr_in clientAddrInfo;
+};
 
 void* handleClient(void* args);
 void* dispatchMessageToAllSocks(void* args);
 
+// Shared across threads
 int sendToAllPipeFds[2];
 struct pollfd fdToPoll;
+int allClientDescriptorsIdx = 0;
+int allClientDescriptors[MAX_SOCKS] = {-1};
 
 int main(int argc, char* argv[]) {
-  int allClientDescriptorsIdx = 0;
-  int allClientDescriptors[MAX_SOCKS] = {-1};
   struct sockaddr_in serverAddrInfo, clientAddrInfo;
+  struct client_thread_info clientInfo;
   int serverDescriptor, clientDescriptor;
 
   serverDescriptor = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -46,28 +54,37 @@ int main(int argc, char* argv[]) {
   printf("Listening ...\n");
   while (1) {
     int len = sizeof(struct sockaddr_in);
-    clientDescriptor = accept(serverDescriptor, (struct sockaddr*) &clientAddrInfo, (socklen_t *)&len);
+    clientDescriptor = accept(
+      serverDescriptor,
+      (struct sockaddr*)&clientAddrInfo,
+      (socklen_t *)&len);
     if (clientDescriptor) {
       allClientDescriptors[allClientDescriptorsIdx] = clientDescriptor;
       allClientDescriptorsIdx++;
+      clientInfo.descriptor = clientDescriptor;
+      clientInfo.clientAddrInfo = clientAddrInfo;
       printf("Got new connection\n");
       // TODO how to join these threads
       pthread_t clientThread;
-      pthread_create(&clientThread, NULL, handleClient, (void*)&clientDescriptor);
+      pthread_create(&clientThread, NULL, handleClient, (void*)&clientInfo);
     }
   }
-  pthread_join(sendToAllThread, NULL);
   return 0;
 }
 
 void* handleClient(void* args) {
-  int clientDescriptor = *((int *)args);
-  printf("Listening to client on descriptor %i\n", clientDescriptor);
+  struct client_thread_info clientThreadInfo = *((struct client_thread_info *)args);
+  char clientAddrString[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &(clientThreadInfo.clientAddrInfo.sin_addr), clientAddrString, INET_ADDRSTRLEN);
+  
+  printf("Listening to client %s on descriptor %i\n", clientAddrString, clientThreadInfo.descriptor);
   char buf[BLEN] = {0}; 
   while(1) {
-    if (recv(clientDescriptor, buf, BLEN, 0) > 0) {
-      printf("Recevied: %s\n", buf);
-      write(sendToAllPipeFds[1], buf, strlen(buf));
+    if (recv(clientThreadInfo.descriptor, buf, BLEN, 0) > 0) {
+      printf("Recevied from (%s): %s\n", clientAddrString, buf);
+      char formattedResponse[BLEN];
+      snprintf(formattedResponse, BLEN, "%s: %s", clientAddrString, buf);
+      write(sendToAllPipeFds[1], formattedResponse, strlen(formattedResponse));
       memset(buf, 0, BLEN);
     }
   }
